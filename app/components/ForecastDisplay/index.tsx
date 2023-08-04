09eed80ebc3f08ebf510300fc2258993452edc02
SquireC <squirec.dev@gmail.com>
2023-08-04 15:50:06 +0100

    feat: add MUI icons

--- app/components/ForecastDisplay/index.tsx
+++ app/components/ForecastDisplay/index.tsx
@@ -1,7 +1,13 @@
 import { ReactElement } from "react";
+import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
 
 const ForecastDisplay = (): ReactElement => {
-  return <>ForecastDisplay</>
+  return (
+    <div>
+      ForecastDisplay
+      <DeviceThermostatIcon /> Temperature 
+    </div>
+  )
 }
 
 export default ForecastDisplay;
